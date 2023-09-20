af5a2dfc4ca05f8eb79a1f920223ed9cf9f44400
AnaJuliaSilverio <ana.silverio@zup.com.br>
2023-09-20 13:52:29 -0300

    ✨ feat: add new querys

--- sistemaBancario/src/main/java/com/ana/sistemaBancario/repositories/AccountRepository.java
+++ sistemaBancario/src/main/java/com/ana/sistemaBancario/repositories/AccountRepository.java
@@ -2,8 +2,15 @@ package com.ana.sistemaBancario.repositories;
 
 import com.ana.sistemaBancario.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
+    @Query("SELECT a FROM Account a WHERE a.client.id = :clientId")
+    List<Account> findAllByClientId(Long clientId);
+    Optional<Account> findByAccountNumber(Long accountNumber);
 }
